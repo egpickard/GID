cf73d0ff124ccc7a2f0502a84e0c430723726553
egpickard <epickard@umass.edu>
2023-05-03 16:24:27 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,39 @@
+import logo from "./logo.png";
+import "./App.css";
+import React, { useState } from "react"; //Source #4
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  const [files, setFiles] = useState([]);
+
+  return (
+    <div className="App">
+      <div className="logo-container">
+        <p>Welcome to the</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <logo-container>
+          <header>
+            <p>
+              Defending childhood, one message at a time - with our advanced
+              grooming detection system
+            </p>
+            <Link to="/begin">
+              <button className="button">Begin</button>
+            </Link>
+            <p>
+              If this is your first time using GID, click the button below to
+              learn how to use it and what file types GID accepts.
+            </p>
+            <p>
+              <Link to="/howto">
+                <button className="button">How to?</button>
+              </Link>
+            </p>
+          </header>
+        </logo-container>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
